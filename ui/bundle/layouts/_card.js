25d9c86cfba6e9f5e320e6bcc7cc4d24ce184b2e
Guillaume Grossetie <ggrossetie@gmail.com>
2018-11-21 19:09:53 +0100

    Fix tag URL

--- ui/bundle/layouts/_card.js
+++ ui/bundle/layouts/_card.js
@@ -2,36 +2,37 @@ const { getTagUrl: getTagUrl } = require('./_tag')
 const { getShortRevisionDate: getShortRevisionDate } = require('./_date')
 
 function getTags (page) {
-  if (page.asciidoc && page.asciidoc.attributes && page.asciidoc.attributes.tags) {
-    return page.asciidoc.attributes.tags.split(',').map(tag => tag.trim())
+  if (page.asciidoc && page.asciidoc.attributes && page.asciidoc.attributes['page-tags']) {
+    return page.asciidoc.attributes['page-tags'].split(',').map(tag => tag.trim())
   }
   return []
 }
 
 module.exports = (uiModel) => {
   return uiModel.page.contents.map((page, index) => {
-    const tagsHTML = getTags(page).map(tag => `<a href="${getTagUrl(tag)}" class="tag is-light has-icon">
+    const tagsHTML = getTags(page).map(tag => `<a href="${getTagUrl(uiModel, tag)}" class="tag is-light has-icon">
   <span class="icon">
     <i class="fas fa-tag"></i> ${tag}
   </span>
 </a>`)
     const attributes = page.asciidoc.attributes
     const image = attributes['page-image']
-    const doctitle = attributes['doctitle']
+    const title = attributes['doctitle']
     const description = attributes['description']
     const revisionDate = new Date(attributes['page-revdate'])
+    const pageUrl = `${uiModel.siteRootPath}/${page.out.path}`
     const imageHTML = image ? `<div class="card-image">
-  <a href="${page.out.rootPath}/${page.out.path}" class="summary">
-    <img src="${uiModel.uiRootPath}/images/${image}" alt="${doctitle} illustration">
+  <a href="${pageUrl}" class="summary">
+    <img src="${uiModel.uiRootPath}/images/${image}" alt="${title} illustration">
   </a>
 </div>` : ''
     return `<div class="column${index === 0 ? ' is-full' : ' is-one-third'}">
   <div class="card article${index === 0 ? ' card-featured' : ''}">
     ${imageHTML}
     <div class="card-content">
-      <a href="${uiModel.siteRootPath}/${page.out.path}" class="summary">
+      <a href="${pageUrl}" class="summary">
         <time datetime="${revisionDate}">${getShortRevisionDate(revisionDate)}</time>
-        <h2 class="article-title">${doctitle}</h2>
+        <h2 class="article-title">${title}</h2>
         <p class="excerpt">${description}</p>
       </a>
       <p class="tags is-condensed">
--- ui/bundle/layouts/_tag.js
+++ ui/bundle/layouts/_tag.js
@@ -1,22 +1,13 @@
-function getTagUrl(tag) {
-  return `./tag/${tag.toLowerCase().replace('.', '-')}/index.html`
+function getTagUrl(uiModel, tag) {
+  return `${uiModel.siteRootPath}/${uiModel.site.homeUrl}tag/${tag.toLowerCase().replace('.', '-')}/index.html`
 }
 
 function tagCloud (uiModel) {
   const tags = uiModel.page.attributes.tags.map(tag => tag.trim())
-  return tags.map(tag => `<a href="${getTagUrl(tag)}" class="tag is-medium">${tag}</a>`)
-}
-
-function tagPost (page) {
-  return page.attributes.tags.map(tag => `<a href="${getTagUrl(tag)}" class="tag is-light has-icon">
-  <span class="icon">
-    <i class="fas fa-tag"></i> ${tag}
-  </span>
-</a>`)
+  return tags.map(tag => `<a href="${getTagUrl(uiModel, tag)}" class="tag is-medium">${tag}</a>`)
 }
 
 module.exports = {
   tagCloud: tagCloud,
-  tagPost: tagPost,
   getTagUrl: getTagUrl
 }
