abb387edf3dfd2e2ab44bfa28338929845f38898
Guillaume Grossetie <ggrossetie@gmail.com>
2018-11-22 22:24:37 +0100

    Use the minified version

--- ui/bundle/layouts/_header.js
+++ ui/bundle/layouts/_header.js
@@ -5,7 +5,7 @@ module.exports = (title, uiModel) => {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>${title}</title>
-  <link rel="stylesheet" href="${uiModel.uiRootPath}/stylesheets/main.css">
+  <link rel="stylesheet" href="${uiModel.uiRootPath}/stylesheets/main.min.css">
   <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>
 <body>
