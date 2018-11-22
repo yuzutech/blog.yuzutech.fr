54456db7b5114bb4e135a9517987436a21d7ffe2
Guillaume Grossetie <ggrossetie@gmail.com>
2018-11-22 22:29:47 +0100

    Force local packages

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# force local packages
+npm install ./packages/asciidoc-loader
+npm install ./packages/ui-loader
+
 antora --html-url-extension-style=indexify --pull site.yml --generator ./lib/generator.js
