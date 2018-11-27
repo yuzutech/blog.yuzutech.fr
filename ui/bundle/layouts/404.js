d3fb09577aeaa98f9c795545084374fdd9694276
Guillaume Grossetie <ggrossetie@gmail.com>
2018-11-27 10:46:33 +0100

    Design a 404 page

--- ui/bundle/layouts/404.js
+++ ui/bundle/layouts/404.js
@@ -1,3 +1,27 @@
-module.exports = () => {
-  return '404'
+const layout = require('./_layout')
+
+function convert (uiModel) {
+  const content = `<section class="hero">
+    <div class="hero-body">
+      <div class="container is-widescreen">
+        <div class="titles">
+          <h1 class="title">
+            <a href="${uiModel.siteRootPath}/index.html">Blog</a>
+            <span class="rss">
+              <a href="${uiModel.siteRootPath}/blog/1.0/rss/feed.xml"><i class="fas fa-rss"></i></a>
+            </span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="section section-404">
+    <div class="container">
+      <h1 class="title">Page not found <i class="far fa-dizzy"></i></h1>
+      <h2 class="subtitle"><i class="fas fa-undo-alt"></i> Back to the <a href="${uiModel.siteRootPath}/index.html">homepage</a></h2>
+    </div>
+  </section>`
+  return layout('Blog - Yuzu tech', content, uiModel)
 }
+
+module.exports = convert
