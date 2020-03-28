3d93a7a853af4c7476b8736fe674874317c5dc33
Guillaume Grossetie <ggrossetie@gmail.com>
2020-03-28 23:06:30 +0100

    Reattach the branch

--- netlify-build.sh
+++ netlify-build.sh
@@ -4,4 +4,8 @@
 npm install ./packages/asciidoc-loader
 npm install ./packages/ui-loader
 
+# reattach the branch!
+git remote add origin $REPOSITORY_URL
+git checkout -b $HEAD
+
 antora --html-url-extension-style=indexify --pull site.yml --generator ./lib/generator.js
