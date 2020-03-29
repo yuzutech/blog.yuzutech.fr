8eaa4067ca92c94d011288522f94c85172fdb4ae
Guillaume Grossetie <ggrossetie@gmail.com>
2020-03-29 01:29:33 +0100

    use echo

--- netlify-build.sh
+++ netlify-build.sh
@@ -5,6 +5,9 @@ npm install ./packages/asciidoc-loader
 npm install ./packages/ui-loader
 
 # debug
-env
+echo $REPOSITORY_URL
+echo $HEAD
+echo $BRANCH
+echo $PULL_REQUEST
 
 antora --html-url-extension-style=indexify --pull site.yml --generator ./lib/generator.js
