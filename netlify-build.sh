7ec55760795c642a2aba7507ecd787a968e8b05d
Guillaume Grossetie <ggrossetie@gmail.com>
2020-03-29 01:31:02 +0100

    debug

--- netlify-build.sh
+++ netlify-build.sh
@@ -4,10 +4,4 @@
 npm install ./packages/asciidoc-loader
 npm install ./packages/ui-loader
 
-# debug
-echo $REPOSITORY_URL
-echo $HEAD
-echo $BRANCH
-echo $PULL_REQUEST
-
 antora --html-url-extension-style=indexify --pull site.yml --generator ./lib/generator.js
--- netlify-reattach-git-branch.sh
+++ netlify-reattach-git-branch.sh
@@ -3,5 +3,14 @@
 # Netlify clone the repository and detach the HEAD
 # Luckily, we can use git metadata to reattach the HEAD
 # https://docs.netlify.com/configure-builds/environment-variables/#git-metadata
+
+# debug
+echo $REPOSITORY_URL
+echo $HEAD
+echo $BRANCH
+echo $PULL_REQUEST
+
+echo "git remote add origin $REPOSITORY_URL"
 git remote add origin $REPOSITORY_URL
+echo "git checkout -b $HEAD"
 git checkout -b $HEAD
