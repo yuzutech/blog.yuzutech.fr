2ec0f26ca814e855119fcf1fc02e220c8fb24e6c
Guillaume Grossetie <ggrossetie@gmail.com>
2020-03-31 15:49:05 +0200

    Delete local branch named "master" before reattach

--- netlify-reattach-git-branch.sh
+++ netlify-reattach-git-branch.sh
@@ -4,4 +4,11 @@
 # Luckily, we can use git metadata to reattach the HEAD
 # https://docs.netlify.com/configure-builds/environment-variables/#git-metadata
 git remote add origin $REPOSITORY_URL
-git checkout -b $HEAD
+if [ $HEAD = "master" ]
+then
+  # since the branch "master" already exists on Netlify,
+  # we first need to remove it to reattach the HEAD
+  # on a new branch named "master"
+  git branch -d master
+fi
+  git checkout -b $HEAD
